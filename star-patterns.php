8e2834b78697f79ea1ee5b825f67c096a481c7f5
ranjitl <ranjitl@cartflows.com>
2021-05-09 01:55:30 +0530

    Created Star Pattern's using PHP.

---
+++ star-patterns.php
@@ -0,0 +1,98 @@
+<?php
+echo "Start pattern : 1";
+echo '<br>';
+
+$no_of_rows = 10;
+for ($i = 0; $i < $no_of_rows; $i++) {
+    for ($j = 0; $j < $no_of_rows ; $j++) { 
+        echo "*";
+    }
+    echo '<br>';
+}
+
+echo "Start pattern : 2";
+echo '<br>';
+
+for ($i = 0; $i < $no_of_rows; $i++) {
+    for ($j = 0; $j <= $i ; $j++) { 
+        echo "*";
+    }
+    echo '<br>';
+}
+
+echo "Start pattern : 3";
+echo '<br>';
+
+for ($i = 0; $i < $no_of_rows; $i++) {
+    for( $j = $no_of_rows; $j > $i; $j-- ) {
+        echo "*";
+    }
+    echo '<br>';
+}
+
+echo "Start pattern : 4";
+echo '<br>';
+// Print Row.
+
+for ($i = 0; $i < $no_of_rows; $i++) { 
+    // Print space.
+    for ($j = $no_of_rows - 1; $j > $i; $j--) { 
+        echo "&nbsp;&nbsp;";
+    }
+    // Print Star.
+    for( $k = 0; $k <= $i; $k++ ) {
+        echo "*";
+    }
+    echo '<br>';
+    // Print star.
+}
+
+echo "Start pattern : 5";
+echo '<br>';
+for ($i = 0; $i < $no_of_rows; $i++) {
+    // Print space.
+    for( $k = 0; $k < $i; $k++ ) {
+        echo "&nbsp;&nbsp;";
+    }
+    // Print Star.
+    for( $j = $no_of_rows; $j > $i; $j-- ) {
+        echo "*";
+    }
+    echo '<br>';
+}
+
+echo "Start pattern : 6";
+echo '<br>';
+
+// Print first half.
+for ($i = 0; $i < $no_of_rows; $i++) { 
+    for ($j = 0; $j <= $i; $j++) { 
+        echo "*";
+    }
+    echo '<br>';
+}
+// Print second half.
+for ($i = 0; $i < $no_of_rows - 1; $i++) { 
+    for ($j = $no_of_rows - 1; $j > $i; $j--) { 
+        echo "*";
+    }
+    echo '<br>';
+}
+
+echo "Start pattern : 7";
+echo '<br>';
+
+// Print first half.
+for ($i = 0; $i < $no_of_rows; $i++) { 
+    for ($j = 0; $j <= $i; $j++) { 
+        echo "*";
+    }
+    echo '<br>';
+}
+// Print second half.
+for ($i = 0; $i < $no_of_rows - 1; $i++) { 
+    for ($j = $no_of_rows - 1; $j > $i; $j--) { 
+        echo "*";
+    }
+    echo '<br>';
+}
